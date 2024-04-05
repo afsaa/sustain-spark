f558559edf583821ce74c9d47aa58a1324456748
Andres Saa <afsaa@utp.edu.co>
2024-04-04 19:53:46 -0500

    Update homepage styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Hero } from '../components/home'
+import { Description, Hero } from '@/components/home'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="w-full h-[calc(100vh-136px)]">
       <Hero />
+      <Description />
     </main>
   )
 }
