5e373d28204ef6a36321b61fbeda3941f1d20718
Andres Saa <afsaa@utp.edu.co>
2024-04-11 12:09:37 -0500

    Create the services folder and add the fake store api service

---
+++ src/services/fakestore/index.ts
@@ -0,0 +1,19 @@
+import { fakeStoreUrls } from './urls'
+
+export const getProducts = async () => {
+  try {
+    const response = await fetch(fakeStoreUrls.products)
+    return response.json()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const getProduct = async (id: number) => {
+  try {
+    const response = await fetch(fakeStoreUrls.product + id)
+    return response.json()
+  } catch (error) {
+    console.error(error)
+  }
+}
---
+++ src/services/fakestore/urls.ts
@@ -0,0 +1,6 @@
+export const fakeStoreUrls = {
+  products: 'https://fakestoreapi.com/products',
+  categories: 'https://fakestoreapi.com/products/categories',
+  product: 'https://fakestoreapi.com/products/',
+  cart: 'https://fakestoreapi.com/carts',
+}
